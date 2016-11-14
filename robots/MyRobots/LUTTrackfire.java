374123b886fe9823a5400bc7f982d351040ce466
andreatjml <a.marti@alumni.ubc.ca>
2016-11-13 16:50:43 -0800

    Trial1

---
+++ robots/MyRobots/LUTTrackfire.java
@@ -0,0 +1,9 @@
+/** Plan of attack - have multiple battle plans. 
+ *  One action per state or multiple actions per state. 
+	
+	Two plans. One - no battle (exploratory) and analyzing. Two - battle like trackfire.
+	Reward: If hit x number of times, give positive reward. Keep using battle. 
+	If miss x number of times, give negative reward. Change battle plans.
+	
+	Exploratory - (1) see enemy, (2) distance - if close, fire hard, if far, either fire small or get closer, (3)  
+ */
