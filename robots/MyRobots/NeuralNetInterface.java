5d41988fe46e7bd0a54db751400fe10b07821947
andreatjml <a.marti@alumni.ubc.ca>
2016-12-11 16:05:08 -0800

    for safekeeping

--- robots/MyRobots/NeuralNetInterface.java
+++ robots/MyRobots/NeuralNetInterface.java
@@ -15,23 +15,23 @@ public interface NeuralNetInterface extends CommonInterface{
 	final double bias = 1.0; 				//input for each neurons bias weight, "final" means that it always contains the same value
 	
 	// abstract method signatures
-	public double binaryActivation(double x); 
+	public double binaryActivation(double x, double sigma); 
 	/*Function name: binaryActivation 
 	 * @param: current hidden value "z"
 	 * @return: new value evaluated at the f(x) = 1/(1 + Math.exp(-x)); 
 	*/ 
-	public double bipolarActivation(double x); 
+	public double bipolarActivation(double x, double sigma); 
 	/*Function name: bipolarActivation 
 	 * @param: current hidden value "z"
 	 * @return: new value evaluated at the f(x) = (2/(1 + e(-x))) - 1 
 	*/ 	
-	public double binaryDerivative(double x); 
+	public double binaryDerivative(double x, double sigma); 
 	/* Function name: binaryDerivative
 	 * @param: input to take the derivative of based on f'(x) = f(x)*(1-f(x)). 
 	 * @return: derivative of value. 
 	 * 
 	 */
-	public double bipolarDerivative(double x); 
+	public double bipolarDerivative(double x, double sigma); 
 	/* Function name: binaryDerivative
 	 * @param: input to take the derivative of. 
 	 * @return: derivative of value: f'(x) =  0.5*(1 + f(x))*(1 - f(x));
--- robots/MyRobots/backPropFinalTest.java
+++ robots/MyRobots/backPropFinalTest.java
@@ -25,23 +25,24 @@ public class backPropFinalTest{
 			 * input data
 			 */
 			/*Initiate variables */
+			int numOutputs = 1; 
 			int numInputs = 9; 		//6 states + 3 actions. 
 			int numHidden = 5;			//number of hidden inputs
 			int numTrials = 8641; 		//number of trials in the training set	
-			double lRate = 0.002; 			//learning rate
-			double errorThreshold = 0.005;	//error threshold
-			double momentum = 0.4;  	//value of momentum 
+			double lRate = 0.02; 			//learning rate
+			double errorThreshold = 0.05;	//error threshold
+			double momentum = 0.2;  	//value of momentum 
 			boolean stopError = false; 	//if flag == false, then stop loop, else continue 
-			int maxEpoch = 1000; 	//if reach maximum number of Epochs, stop loop. 
-			double upperThres = 0.5; 	//upper threshold for random weights
-			double lowerThres = -0.5;	//lower threshold for random weights
+			int maxEpoch = 200; 	//if reach maximum number of Epochs, stop loop. 
+			double upperThres = -1; 	//upper threshold for random weights
+			double lowerThres = 1;	//lower threshold for random weights
 			
 			/* Neural net state action pair*/
 			boolean flag = true;  
 			double [] outputs = new double [8641]; 
 			String [] inputsRaw = new String [8641];
 			String [] inputs1 = new String [8640]; 
-			String [] inputsACT = new String [8640]; 
+			String [] inputsACT = new String [24]; 
 			ArrayList<String> inputToNN = new ArrayList<String>();
 			
 			/* define files to save data */
@@ -87,12 +88,11 @@ public class backPropFinalTest{
 						inputs1[i] = inputsRaw[i].replace("\t", "");				//remove the tab from the line. 
 					}
 					//store the updated actions into a "updatedA" block. 
-					for (int i=0; i < 23; i++){
+					for (int i=0; i < 24; i++){
 						inputsACT[i] = readerACT.readLine();
-						inputs1[i] = inputsRaw[i].replace("\t", "");
+						inputsACT[i] = inputsACT[i].replace("\t", "");
 					}				
-					System.out.println("inputsACT " + Arrays.toString(inputsACT));
-//					System.out.println("output " + Arrays.toString(inputs1));
+//					System.out.println("inputsACT " + Arrays.toString(inputsACT));
 					//can't convert directly to Integer because lose the leading zeros. 
 					//place each string into an array? 
 					for(int i = 0; i < inputs1.length; i++) {
@@ -135,17 +135,13 @@ public class backPropFinalTest{
 				e.printStackTrace();
 			}
 //		    System.out.println( "Whole list=" + inputToNN);
-		    backPropFinal myNeuralNet = new backPropFinal(numInputs, numHidden, lRate, momentum);		/*Create new object of class "myBackProp */
-			myNeuralNet.initializeWeights(upperThres, lowerThres);  							//Initialize weights to random weights between -0.5 and 0.5
-			
+
 			//reformat the input as a string without '[], or space'
 			String [] inputNNRe  = new String [inputToNN.size()];
 			Integer [] inputAsInteger = new Integer[inputToNN.size()];
 			Integer [] firstDigits = new Integer[inputToNN.size()];
-			Integer [] asBinary  = new Integer[inputToNN.size()]; 
-			String [] asString = new String[inputToNN.size()];
 			String [] updatedInputs = new String[inputToNN.size()];
-
+			
 			for (int i = 0; i < inputToNN.size(); i++){
 				inputNNRe[i] = ((String) inputToNN.get(i)).replaceAll("\\D+","");
 				inputAsInteger[i] = Integer.parseInt(inputNNRe[i]);
@@ -156,189 +152,121 @@ public class backPropFinalTest{
 					firstDigits[i] = Integer.parseInt(inputNNRe[i].substring(0, 2));
 				}
 			}
-			System.out.println("input " + Arrays.toString(firstDigits));
 			
-			Integer [] updatedArray = new Integer[3];
-			//for the dimensionality reduction of the states instead of 0-24, goes 00000 --> 11000
-			for (int i = 0; i < inputToNN.size(); i++){
+			for (int i = 0; i <(updatedInputs.length); i++){
 				if (firstDigits[i] == 0){
-					updatedArray[0] = 0; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 0; 
-				} 
+					updatedInputs[i] = inputsACT[0]+inputNNRe[i].substring(1);
+				}
 				else if (firstDigits[i] == 1){
-					updatedArray[0] = 1; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 0; 
-				} 
+					updatedInputs[i] = inputsACT[1]+inputNNRe[i].substring(1);
+				
+				}
 				else if (firstDigits[i] == 2){
-					updatedArray[0] = 2; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 0; 
-				} 
+					updatedInputs[i] = inputsACT[2]+inputNNRe[i].substring(1);
+				}
 				else if (firstDigits[i] == 3){
-					updatedArray[0] = 3; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 0; 
-				} 
+					updatedInputs[i] = inputsACT[3]+inputNNRe[i].substring(1);
+				}
 				else if (firstDigits[i] == 4){
-					updatedArray[0] = 0; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 0; 
-				} 
+					updatedInputs[i] = inputsACT[4]+inputNNRe[i].substring(1);
+				
+				}
 				else if (firstDigits[i] == 5){
-					updatedArray[0] = 1; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 0; 
-				} 
+					updatedInputs[i] = inputsACT[5]+inputNNRe[i].substring(1);
+				}
 				else if (firstDigits[i] == 6){
-					updatedArray[0] = 2; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 0; 
-				} 
+					updatedInputs[i] = inputsACT[6]+inputNNRe[i].substring(1);
+				}
 				else if (firstDigits[i] == 7){
-					updatedArray[0] = 3; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 0; 
-				} 
+					updatedInputs[i] = inputsACT[7]+inputNNRe[i].substring(1);
+				}
 				else if (firstDigits[i] == 8){
-					updatedArray[0] = 0; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 1; 
+					updatedInputs[i] = inputsACT[8]+inputNNRe[i].substring(1);
+				
 				}
 				else if (firstDigits[i] == 9){
-					updatedArray[0] = 1; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 1; 
-				} 
+					updatedInputs[i] = inputsACT[9]+inputNNRe[i].substring(1);
+				}
 				else if (firstDigits[i] == 10){
-					updatedArray[0] = 2; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 1; 
-				} 
+					updatedInputs[i] = inputsACT[10]+inputNNRe[i].substring(2);
+				
+				}
 				else if (firstDigits[i] == 11){
-					updatedArray[0] = 3; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 1; 
-				} 
+					updatedInputs[i] = inputsACT[11]+inputNNRe[i].substring(2);
+				}
 				else if (firstDigits[i] == 12){
-					updatedArray[0] = 0; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 1; 
+					updatedInputs[i] = inputsACT[12]+inputNNRe[i].substring(2);
 				}
 				else if (firstDigits[i] == 13){
-					updatedArray[0] = 1; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 1; 
-				} 
+					updatedInputs[i] = inputsACT[13]+inputNNRe[i].substring(2);
+				
+				}
 				else if (firstDigits[i] == 14){
-					updatedArray[0] = 2; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 1; 
-				} 
+					updatedInputs[i] = inputsACT[14]+inputNNRe[i].substring(2);
+				}
 				else if (firstDigits[i] == 15){
-					updatedArray[0] = 3; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 1; 
-				} 
+					updatedInputs[i] = inputsACT[15]+inputNNRe[i].substring(2);
+				}
 				else if (firstDigits[i] == 16){
-					updatedArray[0] = 0; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 2; 
+					updatedInputs[i] = inputsACT[16]+inputNNRe[i].substring(2);
 				}
 				else if (firstDigits[i] == 17){
-					updatedArray[0] = 1; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 2; 
-				} 
+					updatedInputs[i] = inputsACT[17]+inputNNRe[i].substring(2);
+				}
 				else if (firstDigits[i] == 18){
-					updatedArray[0] = 2; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 2; 
-				} 
+					updatedInputs[i] = inputsACT[18]+inputNNRe[i].substring(2);
+				}
 				else if (firstDigits[i] == 19){
-					updatedArray[0] = 3; 
-					updatedArray[1] = 0; 
-					updatedArray[2] = 2; 
-				} 
+					updatedInputs[i] = inputsACT[19]+inputNNRe[i].substring(2);
+				
+				}
 				else if (firstDigits[i] == 20){
-					updatedArray[0] = 0; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 2; 
+					updatedInputs[i] = inputsACT[20]+inputNNRe[i].substring(2);
 				}
 				else if (firstDigits[i] == 21){
-					updatedArray[0] = 1; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 2; 
-				} 
-				else if (firstDigits[i] == 22){
-					updatedArray[0] = 2; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 2; 
-				} 
-				else if (firstDigits[i] == 23){
-					updatedArray[0] = 3; 
-					updatedArray[1] = 1; 
-					updatedArray[2] = 2; 
-				} 
-//				asBinary[i] = Integer.parseInt(Integer.toBinaryString(firstDigits[i])); 
-//				asString[i] = asBinary[i].toString();
-//				System.out.println(Arrays.toString(updatedArray));
-//				System.out.println(updatedArray[0] + updatedArray[1]);
-				if (inputNNRe[i].length() == 7){
-//					
-					updatedInputs[i] = (updatedArray)+inputNNRe[i].substring(1);
+					updatedInputs[i] = inputsACT[21]+inputNNRe[i].substring(2);
 				}
-//				else if (inputNNRe[i].length() == 8){
-//					updatedInputs[i] = updatedArray[0] + updatedArray[1] + updatedArray[2]+inputNNRe[i].substring(2);
-//				}
-				
-			}
-			//not all the inputs are sized 11. 
-//			System.out.println("input " + Arrays.toString(updatedInputs));
-//			for (int i = 0; i < (updatedInputs.length); i++){
-//				if (updatedInputs[i].length() < 11){
-//					//add a zero to the beginning until string is length 11. 
-//					int getLength = updatedInputs[i].length(); 
-//					if (getLength == 7){
-//						updatedInputs[i] = "0000"+updatedInputs[i].substring(0);
-////						System.out.println("updatedInputs" + updatedInputs[i]); 
-//					}
-//					else if (getLength == 8){
-//						updatedInputs[i] = "000"+updatedInputs[i].substring(0);
-////						System.out.println("updatedInputs" + updatedInputs[i]); 
-//					}
-//					else if (getLength == 9){
-//						updatedInputs[i] = "00"+updatedInputs[i].substring(0);
-////						System.out.println("updatedInputs" + updatedInputs[i]); 
-//					}
-//					else if (getLength == 10){
-//						updatedInputs[i] = "0"+updatedInputs[i].substring(0);
-////						System.out.println("updatedInputs" + updatedInputs[i]); 
-//					}
-//				}
-//				System.out.println("updatedInputs " + updatedInputs[i]); 	
-//				saveFile2.println("StateActionVector \t " + updatedInputs[i]); 
-//			}	
-			saveFile2.close(); 
+				else if (firstDigits[i] == 22){
+					updatedInputs[i] = inputsACT[22]+inputNNRe[i].substring(2);
+				}
+				else if (firstDigits[i] == 23){
+					updatedInputs[i] = inputsACT[23]+inputNNRe[i].substring(2);
+				}
+//				saveFile2.println("StateActionVector \t " + updatedInputs[i]);
+			} 	
+			
+			/* preprocess outputs 
+			 * 
+			 */
+			
 //			System.out.println("output " + Arrays.toString(outputs));
+			double qMax = outputs[0];
+			for (int i = 1; i < outputs.length; i++) {
+			    if (outputs[i] > qMax) {
+			      qMax = outputs[i];
+			    }
+			}
+			double qMin = outputs[0];
+			for (int i = 1; i < outputs.length; i++) {
+			    if (outputs[i] < qMin) {
+			    	qMin = outputs[i];
+			    }
+			}
+			
+			/* initialize myNeuralNet. 
+			 * */
+		    backPropFinal myNeuralNet = new backPropFinal(numInputs, numHidden, lRate, momentum, qMax, qMin, numInputs, numHidden, numOutputs);		/*Create new object of class "myBackProp */
+			myNeuralNet.initializeWeights(upperThres, lowerThres);  							//Initialize weights to random weights between -0.5 and 0.5
+			
 			/* Start epochs */  
-//			int numEpoch = 0; 
-//			double errorTemp = 0; 
-//			while (stopError == false){ 
-//				double totalError = 0.0;
-////				numTrials = 4; 		//for testing
-//				for (int i = 0; i < (numTrials-1); i++){
-//					if (outputs[i] == 0.0){
-//						continue;
-//						
-//					}
+			int numEpoch = 0; 
+			double errorTemp = 0; 
+			while (stopError == false){ 
+				double totalError = 0.0;
+				for (int i = 0; i < (numTrials-1); i++){
 //					//Call function for forward propagation
-////					System.out.println("input " + (updatedInputs[i]));
-////					System.out.println("output " + (outputs[i]));
-//					//should we eliminate the outputs that give only zero value? 
 //					double[] Ycalc = myNeuralNet.outputForward(updatedInputs[i], flag, i);
-////					System.out.println("YCalc " + Arrays.toString(Ycalc));
+//					System.out.println("YCalc " + Arrays.toString(Ycalc));
 ////					//Call function for backward propagation
 ////					System.out.println("outputs[i] " + outputs[i]);
 ////					System.out.println("YCalc " + inputNNRe[i]);
@@ -347,23 +275,20 @@ public class backPropFinalTest{
 ////					errorTemp = (java.lang.Math.pow(error, 2)); 
 //					totalError += error;
 //					saveFile2.println("StateActionVector \t " + updatedInputs[i] + "\t error \t " + error);
-//					
-//				}
+				}
 //				saveFile2.close();	
 //				//rmsError = sqrt(sumof(target - actual output)^2)/numPatterns)
-////				System.out.println("error Temp " + errorTemp);
-//				double rmsError = Math.sqrt(errorTemp)/numTrials; 
-//////				System.out.println("totalError " + totalError);
-//				System.out.println("numEpoch " + numEpoch);
+//				double rmsError = Math.sqrt(totalError)/numTrials; 
+////				System.out.println("rmsError " + rmsError);
+////				System.out.println("numEpoch " + numEpoch);
 //				if (numEpoch > maxEpoch){
 //					System.out.println("Trial " + a + "\tEpoch " + numEpoch);
 //					stopError = true;
-//					
 //				}		
-////				System.out.println("rmsError " + rmsError);
-//				myNeuralNet.save (rmsError, numEpoch, saveFile1, stopError);
+//				System.out.println("numTrial " + numEpoch);
+////				myNeuralNet.save (rmsError, numEpoch, saveFile1, stopError);
 //				numEpoch +=1;
-//			}			
+			}			
 		}
 	}
 }
